774299256bc8c4fd8f22d672cb8ab712afbaecdf
gekui.wu <2845479857@qq.com>
2024-04-19 18:07:58 +0800

    emmmmmm

--- DataDefFunc_20240419/script/test_login.py
+++ DataDefFunc_20240419/script/test_login.py
@@ -1,15 +1,13 @@
 import sys
-sys.path.append("..")
+sys.path.append("../service")
 
 syspath=sys.path
 print(syspath)
 
-from DataDefFunc_20240419.service.login_service import login
+# from DataDefFunc_20240419.service.login_service import login
 
-login('username','123','bbbb')
-login('admin','123456','888')
-login('99','1456','bbbb')
-
-
+# login('username','123','bbbb')
+# login('admin','123456','8888')
+# login('99','1456','bbbb')
 
 
