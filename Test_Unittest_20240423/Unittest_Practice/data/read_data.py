77fc28a9084379209f33eeec5a4bafb8ba5b3e79
28454 <2845479857@qq.com>
2024-04-23 22:29:14 +0800

    tuple(i.values())

--- Test_Unittest_20240423/Unittest_Practice/data/read_data.py
+++ Test_Unittest_20240423/Unittest_Practice/data/read_data.py
@@ -7,10 +7,12 @@ def read_data_method():
         data = json.load(f)
         list_data = []
         for i in data:
-            password = i['password']
-            username = i['username']
-            desc = i['desc']
-            expect = i['expect']
-            list_data.append([desc,username,password,expect])
+            # password = i['password']
+            # username = i['username']
+            # desc = i['desc']
+            # expect = i['expect']
+            # list_data.append([desc,username,password,expect])
+            list_data.append(tuple(i.values())) # 简化写法将数组中的每个字典中的values
+            # 转换为元组后append到数组中
 
         return list_data
