a656bca5d8214010584fb4661c3698f21f1b9445
28454 <2845479857@qq.com>
2024-04-29 23:12:15 +0800

    find ele

---
+++ UiSeleniumApp/find_element.md
@@ -0,0 +1,30 @@
+## 定位方法
+
+### id定位
+* 方法：driver.find_element(By.ID, 'username')   
+* 前置：标签必须有id属性
+### name定位
+* 方法：driver.find_element(By.NAME,'username')
+* 前置：标签必须有name属性
+* 提示：由于name属性值可以重复，所以使⽤时需要查看是否为唯⼀。
+### class定位
+* 方法：driver.find_element(By.CLASS_NAME,"inp").send_keys("2845479857@qq.com")
+* 前置：标签必须有class属性
+* 提示：由于class属性值可以有多个值,用空格隔开，默认用第一个
+`如果标签有多个class值，使⽤任何⼀个都可以。如：c`
+### tag name定位
+* 说明：可以根据标签名进行定位
+* 方法：driver.find_element(By.CLASS_NAME,"inp")
+* 提示：如果⻚⾯存在多个相同标签，默认返回第⼀个。
+### link text 定位
+* 说明： 根据链接⽂本(a标签)定位
+* ⽅法： driver.find_element(By.LINK_TEXT,"链接⽂本")
+* ://rutracker.in/https://rutracker.in/
+* 特点： 传⼊的链接⽂本，必须全部匹配，不能模糊
+### partial_link_test 定位
+* 说明： 根据链接⽂本(a标签)定位
+* ⽅法： driver.find_element(By.PARTIAL_LINK_TEXT,"链接⽂本")
+* 特点： 传⼊的链接⽂本，⽀持模糊匹配（传⼊局部⽂字）
+### 扩展查找一组元素
+* 说明：返回列表格式，要使⽤需要添加下标或遍历。
+* 提示： ⼋⼤元素定位⽅法，都可以使⽤⼀组元素定位,如果没有搜索到符合标签，返回空列表
--- UiSeleniumApp/location.py
+++ UiSeleniumApp/location.py
@@ -18,6 +18,7 @@
 from time import sleep
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # # 1、打开⾕歌浏览器(实例化浏览器对象)
 # driver = webdriver.Chrome()
@@ -29,22 +30,44 @@ from selenium import webdriver
 
 """
 3.1 id定位
-⽅法： driver.find_element_by_id("id值")
+⽅法： driver.find_element(By.ID, 'username')
 前置： 标签必须有id属性
 输⼊⽅法： 元素.send_keys("内容")
 """
 
 # 1、获取浏览器
-driver = webdriver.Chrome()
-# 2、打开url
-driver.get("http://192.168.42.129/Home/user/login.html/")
-# 3、查找操作元素
-# ⽤户名 -> id ->driver.find_element_by_id("id")
-# 元素.send_keys()输⼊⽅法
+# driver = webdriver.Chrome()
+# # 2、打开url
+# driver.get("http://192.168.254.131/Home/user/login.html/")
+# # 3、查找操作元素
+# # ⽤户名 -> id ->driver.find_element_by_id("id")
+# # 元素.send_keys()输⼊⽅法
+#
+# driver.find_element(By.ID, 'username').send_keys("admin")
+# # 密码
+# driver.find_element("id","password").send_keys("123456")
+# # 4、关闭浏览器
+# sleep(5)
+#
+# driver.quit()
 
-driver.find_element("id","username").send_keys("admin")
-# 密码
-driver.find_element("id","password").send_keys("123456")
-# 4、关闭浏览器
-sleep(5)
+# 1、获取浏览器
+driver = webdriver.Chrome()
+# # 2、打开url
+# driver.get("http://192.168.254.131/Home/user/login.html/")
+
+# driver.find_element(By.NAME,'username').send_keys("admin")
+# driver.find_element(By.NAME,'password').send_keys("123456")
+
+# driver.get("http://192.168.254.131/Home/User/reg/t/email.html")
+# driver.find_element(By.CLASS_NAME,"inp").send_keys("2845479857@qq.com")
+
+driver.get("http://192.168.254.131/Home/User/reg/t/email.html")
+driver.find_element(By.TAG_NAME,"inp").send_keys("2845479857@qq.com")
+
+driver.find_element(By.LINK_TEXT,"aaa").send_keys("2845479857@qq.com")
+driver.find_element(By.PARTIAL_LINK_TEXT,"bbbb").send_keys("2845479857@qq.com")
+
+
+sleep(20)
 driver.quit()
