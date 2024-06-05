1412dd2759c26edd18a94e36a9e90b8135056bb7
gekui.wu <2845479857@qq.com>
2024-06-05 18:10:28 +0800

    20240605 Pytest自动化测试框架搭建1-1

---
+++ InterfaceTest_PyTest/utils/read_data_yaml.py
@@ -0,0 +1,10 @@
+import pytest
+import yaml
+
+f = open("../config/data.yaml", "r", encoding="utf-8")
+data = yaml.safe_load(f)
+print(data["hero"])
+print(data["hero_name"])
+print(data["heros"])
+print(data["hero_name_list"])
+
